1eb3b435ce7481f2e74ffc59a41c93b4c24bd832
mohit-prajapati1 <mohitprajapati.one@gmail.com>
2026-02-26 14:26:12 +0530

    First change from Pycharm

---
+++ config.py
@@ -0,0 +1,3 @@
+RAW_DATA_PATH = "data/raw/"
+PROCESSED_DATA_PATH = "data/Processed/"
+LOG_FILE = "Logs/etl.log"
---
+++ utils.py
@@ -0,0 +1,7 @@
+# utils.py (Reusable Components)
+import pandas as pd
+import  logging
+from datetime import datetime
+
+def setup_logger(log_file):
+    
